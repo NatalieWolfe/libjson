a8345cda2a3066d5a163573286edd9992cea9e04
Natalie Wolfe <natalie@lifewanted.com>
2014-10-24 17:48:47 -0700

    Fixed build issues.

--- _internal/TestSuite2/JSONValidator/securityTest.cpp
+++ _internal/TestSuite2/JSONValidator/securityTest.cpp
@@ -7,21 +7,23 @@ void testJSONValidator__securityTest::testsecurity(void){
 		#if (JSON_SECURITY_MAX_NEST_LEVEL != 128)
 			#error, test suite only wants a nest security level of 100
 		#endif
-		{
-			json_string json(JSON_TEXT("{"));
-			for(unsigned int i = 0; i < 127; ++i){
-				json += JSON_TEXT("\"n\":{");
+		#ifdef JSON_VALIDATE
+			{
+				json_string json(JSON_TEXT("{"));
+				for(unsigned int i = 0; i < 127; ++i){
+					json += JSON_TEXT("\"n\":{");
+				}
+				json += json_string(128, '}');
+				assertTrue(JSONValidator::isValidRoot(json.c_str()));
 			}
-			json += json_string(128, '}');
-			assertTrue(JSONValidator::isValidRoot(json.c_str()));
-		}
-		{
-			json_string json(JSON_TEXT("{"));
-			for(unsigned int i = 0; i < 128; ++i){
-				json += JSON_TEXT("\"n\":{");
+			{
+				json_string json(JSON_TEXT("{"));
+				for(unsigned int i = 0; i < 128; ++i){
+					json += JSON_TEXT("\"n\":{");
+				}
+				json += json_string(129, '}');
+				assertFalse(JSONValidator::isValidRoot(json.c_str()));
 			}
-			json += json_string(129, '}');
-			assertFalse(JSONValidator::isValidRoot(json.c_str()));
-		}
+		#endif
 	#endif
 }
