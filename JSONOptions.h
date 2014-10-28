6097f8e975bf91c5042ca9456a6f9757da31a665
Natalie Wolfe <natalie@lifewanted.com>
2014-10-27 18:37:33 -0700

    Added @def

--- JSONOptions.h
+++ JSONOptions.h
@@ -1,12 +1,16 @@
 #pragma once
 
 /**
+ * @def JSON_STRICT
+ *
  * JSON_STRICT removes all of libjson's extensions.  Meaning no comments, no special numbers
  */
 //#define JSON_STRICT
 
 
 /**
+ * @def JSON_DEBUG
+ *
  * JSON_DEBUG is used to perform extra error checking.  Because libjson usually
  * does on the fly parsing, validation is impossible, so this option will allow
  * you to register an error callback so that you can record what is going wrong
@@ -18,6 +22,8 @@
 
 
 /**
+ * @def JSON_SAFE
+ *
  * JSON_SAFE performs similarly to JSON_DEBUG, except this option does protect
  * from the errors that it encounters.  This option is recommended for those who
  * feel it's possible for their program to encounter invalid json.
@@ -26,6 +32,8 @@
 
 
 /**
+ * @def JSON_STDERROR
+ *
  * JSON_STDERROR routes error messages to cerr instead of a callback, this
  * option hides the callback registering function.  This will usually display
  * messages in the console
@@ -34,6 +42,8 @@
 
 
 /**
+ * @def JSON_PREPARSE
+ *
  * JSON_PREPARSE causes all parsing to be done immediately.  By default, libjson
  * parses nodes on the fly as they are needed, this makes parsing much faster if
  * your program gets a lot of information that it doesn't need.  An example of
@@ -44,6 +54,8 @@
 
 
 /**
+ * @def JSON_LESS_MEMORY
+ *
  * JSON_LESS_MEMORY will force libjson to let go of memory as quickly as it can
  * this is recommended for software that has to run on less than optimal machines.
  * It will cut libjson's memory usage by about 20%, but also run slightly slower.
@@ -54,6 +66,8 @@
 
 
 /**
+ * @def JSON_UNICODE
+ *
  * JSON_UNICODE tells libjson to use wstrings instead of regular strings, this
  * means that libjson supports the full array of unicode characters, but also takes
  * much more memory and processing power.
@@ -62,6 +76,8 @@
 
 
 /**
+ * @def JSON_REF_COUNT
+ *
  * JSON_REF_COUNT causes libjson to reference count JSONNodes, which makes copying
  * and passing them around much faster.  It is recommended that this stay on for
  * most uses
@@ -70,6 +86,8 @@
 
 
 /**
+ * @def JSON_BINARY
+ *
  * JSON_BINARY is used to support binary, which is base64 encoded and decoded by libjson,
  * if this option is not turned off, no base64 support is included
  */
@@ -77,6 +95,8 @@
 
 
 /**
+ * @def JSON_EXPOSE_BASE64
+ *
  * JSON_EXPOSE_BASE64 is used to turn on the functionality of libjson's base64 encoding
  * and decoding.  This may be useful if you want to obfuscate your json, or send binary data over
  * a network
@@ -85,6 +105,8 @@
 
 
 /**
+ * @def JSON_ITERATORS
+ *
  * JSON_ITERATORS turns on all of libjson's iterating functionality.  This would usually
  * only be turned off while compiling for use with C
  */
@@ -92,6 +114,8 @@
 
 
 /**
+ * @def JSON_STREAM
+ *
  * JSON_STREAM turns on libjson's streaming functionality.  This allows you to give parts of
  * your json into a stream, which will automatically hit a callback when full nodes are
  * completed
@@ -100,6 +124,8 @@
 
 
 /**
+ * @def JSON_MEMORY_CALLBACKS
+ *
  * JSON_MEMORY_CALLBACKS exposes functions to register callbacks for allocating, resizing,
  * and freeing memory.  Because libjson is designed for customizability, it is feasible
  * that some users would like to further add speed by having the library utilize a memory
@@ -110,6 +136,8 @@
 
 
 /**
+ * @def JSON_MEMORY_MANAGE
+ *
  * JSON_MEMORY_MANAGE is used to create functionality to automatically track and clean
  * up memory that has been allocated by the user.  This includes strings, binary data, and
  * nodes.  It also exposes bulk delete functions.
@@ -118,6 +146,8 @@
 
 
 /**
+ * @def JSON_MEMORY_POOL
+ *
  * JSON_MEMORY_POOL Turns on libjson's iteraction with mempool++.  It is more efficient that simply
  * connecting mempool++ to the callbacks because it integrates things internally and uses a number
  * of memory pools.  This value tells libjson how large of a memory pool to start out with.  500KB
@@ -128,6 +158,8 @@
 
 
 /**
+ * @def JSON_MUTEX_CALLBACKS
+ *
  * JSON_MUTEX_CALLBACKS exposes functions to register callbacks to lock and unlock
  * mutexs and functions to lock and unlock JSONNodes and all of it's children.  This
  * does not prevent other threads from accessing the node, but will prevent them from
@@ -139,6 +171,8 @@
 
 
 /**
+ * @def JSON_MUTEX_MANAGE
+ *
  * JSON_MUTEX_MANAGE lets you set mutexes and forget them, libjson will not only keep
  * track of the mutex, but also keep a count of how many nodes are using it, and delete
  * it when there are no more references
@@ -147,12 +181,16 @@
 
 
 /**
+ * @def JSON_OCTAL
+ *
  * JSON_OCTAL allows libjson to use octal values in numbers.
  */
 //#define JSON_OCTAL
 
 
 /**
+ * @def JSON_WRITE_PRIORITY
+ *
  * JSON_WRITE_PRIORITY turns on libjson's writing capabilties.  Without this libjson can only
  * read and parse json, this allows it to write back out.  Changing the value of the writer
  * changes how libjson compiles, and how fast it will go when writing
@@ -161,6 +199,8 @@
 
 
 /**
+ * @def JSON_READ_PRIORITY
+ *
  * JSON_READ_PRIORITY turns on libjson's reading capabilties.  Changing the value of the reader
  * changes how libjson compiles, and how fast it will go when reading
  */
@@ -168,6 +208,8 @@
 
 
 /**
+ * @def JSON_NEWLINE
+ *
  * JSON_NEWLINE affects how libjson writes.  If this option is turned on, libjson
  * will use whatever it's defined as for the newline signifier, otherwise, it will use
  * standard unix \n.
@@ -176,6 +218,8 @@
 
 
 /**
+ * @def JSON_INDENT
+ *
  * JSON_INDENT affects how libjson writes.  If this option is turned on, libjson
  * will use \t to indent formatted json, otherwise it will use the number of characters
  * that you specify.  If this is not turned on, then it will use the tab (\t) character
@@ -184,6 +228,8 @@
 
 
 /**
+ * @def JSON_ESCAPE_WRITES
+ *
  * JSON_ESCAPE_WRITES tells the libjson engine to escape special characters when it writes
  * out.  If this option is turned off, the json it outputs may not adhere to JSON standards
  */
@@ -191,6 +237,8 @@
 
 
 /**
+ * @def JSON_COMMENTS
+ *
  * JSON_COMMENTS tells libjson to store and write comments.  libjson always supports
  * parsing json that has comments in it as it simply ignores them, but with this option
  * it keeps the comments and allows you to insert further comments
@@ -199,6 +247,8 @@
 
 
 /**
+ * @def JSON_WRITE_BASH_COMMENTS
+ *
  * JSON_WRITE_BASH_COMMENTS will cause libjson to write all comments in bash (#) style
  * if this option is not turned on, then it will use C-style comments.  Bash comments are
  * all single line
@@ -207,6 +257,8 @@
 
 
 /**
+ * @def JSON_WRITE_SINGLE_LINE_COMMENTS
+ *
  * JSON_WRITE_SINGLE_LINE_COMMENTS will cause libjson to write all comments in using //
  * notation, or (#) if that option is on.  Some parsers do not support multiline C comments
  * although, this option is not needed for bash comments, as they are all single line anyway
@@ -215,6 +267,8 @@
 
 
 /**
+ * @def JSON_ARRAY_SIZE_ON_ONE_LINE
+ *
  * JSON_ARRAY_SIZE_ON_ON_LINE allows you to put small arrays of primitives all on one line
  * in a write_formatted.  This is common for tuples, like coordinates.  If must be defined
  * as an integer
@@ -223,12 +277,16 @@
 
 
 /**
+ * @def JSON_VALIDATE
+ *
  * JSON_VALIDATE turns on validation features of libjson.
  */
 //#define JSON_VALIDATE
 
 
 /**
+ * @def JSON_CASE_INSENSITIVE_FUNCTIONS
+ *
  * JSON_CASE_INSENSITIVE_FUNCTIONS turns on funtions for finding child nodes in a case-
  * insenititve way
  */
@@ -236,6 +294,8 @@
 
 
 /**
+ * @def JSON_INDEX_TYPE
+ *
  * JSON_INDEX_TYPE allows you to change the size type for the children functions. If this
  * option is not used then unsigned int is used.  This option is useful for cutting down
  * on memory, or using huge numbers of child nodes (over 4 billion)
@@ -244,6 +304,8 @@
 
 
 /**
+ * @def JSON_BOOL_TYPE
+ *
  * JSON_BOOL_TYPE lets you change the bool type for the C interface.  Because before C99 there
  * was no bool, and even then it's just a typedef, you may want to use something else.  If this
  * is not defined, it will revert to int
@@ -252,6 +314,8 @@
 
 
 /**
+ * @def JSON_INT_TYPE
+ *
  * JSON_INT_TYPE lets you change the int type for as_int.  If you ommit this option, the default
  * long will be used
  */
@@ -259,6 +323,8 @@
 
 
 /**
+ * @def JSON_NUMBER_TYPE
+ *
  * JSON_NUMBER_TYPE lets you change the number type for as_float as well as the internal storage for the
  *   number.  If you omit this option, the default double will be used for most cases and float for JSON_LESS_MEMORY
  */
@@ -266,6 +332,8 @@
 
 
 /**
+ * @def JSON_STRING_HEADER
+ *
  * JSON_STRING_HEADER allows you to change the type of string that libjson uses both for the
  * interface and internally.  It must implement most of the STL string interface, but not all
  * of it.  Things like wxString or QString should wourk without much trouble
@@ -274,6 +342,8 @@
 
 
 /**
+ * @def JSON_UNIT_TEST
+ *
  * JSON_UNIT_TEST is used to maintain and debug the libjson.  It makes all private
  * members and functions public so that tests can do checks of the inner workings
  * of libjson.  This should not be turned on by end users.
@@ -282,6 +352,8 @@
 
 
 /**
+ * @def JSON_NO_EXCEPTIONS
+ *
  * JSON_NO_EXCEPTIONS turns off any exception throwing by the library.  It may still use exceptions
  * internally, but the interface will never throw anything.
  */
@@ -289,6 +361,8 @@
 
 
 /**
+ * @def JSON_DEPRECATED_FUNCTIONS
+ *
  * JSON_DEPRECATED_FUNCTIONS turns on functions that have been deprecated, this is for backwards
  * compatibility between major releases.  It is highly recommended that you move your functions
  * over to the new equivalents
@@ -297,6 +371,8 @@
 
 
 /**
+ * @def JSON_CASTABLE
+ *
  * JSON_CASTABLE allows you to call as_bool on a number and have it do the 0 or not 0 check,
  * it also allows you to ask for a string from a number, or boolean, and have it return the right thing.
  * Without this option, those types of requests are undefined.  It also exposes the as_array, as_node, and cast
@@ -306,6 +382,8 @@
 
 
 /**
+ * @def JSON_SECURITY_MAX_NEST_LEVEL
+ *
  * JSON_SECURITY_MAX_NEST_LEVEL is a security measure added to make prevent against DoS attacks
  * This only affects validation, as if you are worried about security attacks, then you are
  * most certainly validating json before sending it to be parsed.  This option allows you to limit how many
@@ -315,6 +393,8 @@
 
 
 /**
+ * @def JSON_SECURITY_MAX_STRING_LENGTH
+ *
  * JSON_SECURITY_MAX_STRING_LENGTH is another security measure, preventing DoS attacks with very long
  * strings of JSON.  32MB is the default value for this, this allows large images to be embedded
  */
@@ -322,6 +402,8 @@
 
 
 /**
+ * @def JSON_SECURITY_MAX_STREAM_OBJECTS
+ *
  * JSON_SECURITY_MAX_STREAM_OBJECTS is a security measure for streams.  It prevents DoS attacks with
  * large number of objects hitting the stream all at once.  128 is a lot of objects, but not out of
  * the question for high speed systems.
